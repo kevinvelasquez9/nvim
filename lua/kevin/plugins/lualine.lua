1b2bee850e0089def073a71217369c0b95be2979
Kevin Velasquez <60550789+kevinvelasquez9@users.noreply.github.com>
2022-11-06 21:18:17 -0500

    added lsp name to lualine

--- lua/kevin/plugins/lualine.lua
+++ lua/kevin/plugins/lualine.lua
@@ -3,11 +3,20 @@ if not status then
 	return
 end
 
+local function lsp_name()
+	for _, client in ipairs(vim.lsp.get_active_clients()) do
+		if client.attached_buffers[vim.api.nvim_get_current_buf()] then
+			return client.name
+		end
+	end
+	return ""
+end
+
 lualine.setup({
 	options = {
 		theme = "tokyonight",
 	},
 	sections = {
-		lualine_x = { "filetype" },
+		lualine_x = { "filetype", lsp_name },
 	},
 })
