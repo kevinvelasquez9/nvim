efdf6e1c5972b6518166f35bb8d70dddcec95f3f
Kevin Velasquez <60550789+kevinvelasquez9@users.noreply.github.com>
2022-12-19 15:56:40 -0500

    updating packer bootstrap

--- lua/kevin/plugins.lua
+++ lua/kevin/plugins.lua
@@ -1,29 +1,12 @@
-local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+	is_bootstrap = true
+	vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+	vim.cmd([[packadd packer.nvim]])
 end
 
-local packer_bootstrap = ensure_packer()
-
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
-local status, packer = pcall(require, "packer")
-if not status then
-	return
-end
-
-return packer.startup(function(use)
+require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
 
 	-- colorscheme
@@ -79,7 +62,24 @@ return packer.startup(function(use)
 		},
 	})
 
-	if packer_bootstrap then
+	if is_bootstrap then
 		require("packer").sync()
 	end
 end)
+
+if is_bootstrap then
+	print("==================================")
+	print("    Plugins are being installed   ")
+	print("    Wait until Packer completes,  ")
+	print("       then restart nvim          ")
+	print("==================================")
+	return
+end
+
+-- automatically source and re-compile packer when you save this file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
