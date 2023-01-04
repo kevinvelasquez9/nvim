0a351e68a23c6c673b5d65bb1cbd4b2494d04baa
Kevin Velasquez <60550789+kevinvelasquez9@users.noreply.github.com>
2023-01-04 07:18:34 -0500

    separated null-ls and lsp configs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,8 +15,6 @@ local M = {
 		-- snippets
 		"L3MON4D3/LuaSnip",
 		"rafamadriz/friendly-snippets",
-		-- formatting
-		"jose-elias-alvarez/null-ls.nvim",
 	},
 }
 
@@ -26,16 +24,7 @@ function M.config()
 
 	lsp.preset("recommended")
 
-	lsp.ensure_installed({
-		"cssls",
-		"gopls",
-		"html",
-		"ocamllsp",
-		"pyright",
-		"sumneko_lua",
-		"tailwindcss",
-		"tsserver",
-	})
+	lsp.ensure_installed({ "sumneko_lua" })
 
 	local cmp_kinds = {
 		-- https://github.com/microsoft/vscode-codicons/raw/main/dist/codicon.ttf
@@ -98,44 +87,6 @@ function M.config()
 	})
 
 	lsp.setup()
-
-	local null_ls = require("null-ls")
-	local formatting = null_ls.builtins.formatting
-	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-	null_ls.setup({
-		sources = {
-			formatting.black,
-			formatting.gofumpt,
-			formatting.google_java_format,
-			formatting.ocamlformat.with({
-				args = { "--enable-outside-detected-project", "--name", "$FILENAME", "-" },
-			}),
-			formatting.prettier,
-			formatting.rustfmt,
-			formatting.stylua,
-			formatting.taplo,
-		},
-		-- format on save
-		on_attach = function(current_client, bufnr)
-			if current_client.supports_method("textDocument/formatting") then
-				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-				vim.api.nvim_create_autocmd("BufWritePre", {
-					group = augroup,
-					buffer = bufnr,
-					callback = function()
-						vim.lsp.buf.format({
-							filter = function(client)
-								--  only use null-ls for formatting instead of lsp server
-								return client.name == "null-ls"
-							end,
-							bufnr = bufnr,
-						})
-					end,
-				})
-			end
-		end,
-	})
 end
 
 return M
